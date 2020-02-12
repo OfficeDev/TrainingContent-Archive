e756013581fc6abc83826248e2ee03d3018531bf
Andrew Connell <me@andrewconnell.com>
2020-02-12 18:19:52 -0500

    Adding archived module O3654

---
+++ O3654/O3654-4 Android/03 O365 Exc SDK/Completed Projects/Exercise 2/ExchangeApp/app/src/main/java/com/example/o365testapp/Constants.java
@@ -0,0 +1,9 @@
+package com.example.o365testapp;
+
+public final class Constants {
+    public static final String RESOURCE = "https://graph.microsoft.com/";
+
+    public static final String CLIENT_ID = "YOUR CLIENT ID";
+
+    public static final String REDIRECT_URI = "http://example.com/redirect";
+}
