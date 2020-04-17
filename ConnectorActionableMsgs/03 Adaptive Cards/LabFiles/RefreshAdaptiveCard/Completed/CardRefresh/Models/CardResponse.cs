c1624d1907bd3c3ec36c33fe3051514ad1dbbbac
Andrew Connell <me@andrewconnell.com>
2020-04-17 11:13:33 -0400

    Add archived Connector/ActionableMessages modules
    - added modules are outdated and replaced by new module in PR: https://github.com/OfficeDev/TrainingContent/pull/692

---
+++ ConnectorActionableMsgs/03 Adaptive Cards/LabFiles/RefreshAdaptiveCard/Completed/CardRefresh/Models/CardResponse.cs
@@ -0,0 +1,17 @@
+﻿/*
+* Copyright (c) Microsoft Corporation. All rights reserved.
+* Licensed under the MIT license.
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CardRefresh.Models
+{
+  public class CardResponse
+  {
+    public string Comment { get; set; }
+    public string CachedComments { get; set; }
+  }
+}
